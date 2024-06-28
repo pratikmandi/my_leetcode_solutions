f97110c3116a3410804dfe9c28ace4d77ca9b3ce
pratikmandi <pratikmandi0984@gmail.com>
2024-06-28 14:55:11 +0530

    Added two ways to solve the problem

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    vector<int> chars(128);
- 
-        int left=0;
-        int right=0;
- 
-        int res=0;
-        while (right<s.length()) {
-            char r=s[right];
-            chars[r]++;
-            while (chars[r]>1) {
-                char l=s[left];
-                chars[l]--;
-                left++;
+    //Method 1:
+        vector<int> hashMap(256,-1);
+        int l=0,r=0,maxlength=0;
+        int n=s.length();
+
+        while(r<n){
+            if(hashMap[s[r]!= -1]){
+                if(hashMap[s[r]]>=l){
+                    l=hashMap[s[r]]+1;
+                }
             }
-            res = max(res,right-left+1);
-            right++;
+            int len=r-l+1;
+            maxlength=max(len,maxlength);
+            hashMap[s[r]]=r;
+            r++;
         }
- 
-        return res;
+        return maxlength;
     }
+    
+    //Method 2:
+//         if(s.length() == 0)
+//             return 0;
+
+//         unordered_map<char,int> mp;
+//         int i = 0, j = 0;
+//         int n = s.length();
+//         int mx = INT_MIN;
+
+//         while(j<n){
+//             mp[s[j]]++;
+
+//             if(mp.size()==j-i+1){ // All characters are unique
+//                 mx=max(mx,j-i+1);
+//                 j++;
+//             }
+//             else if(mp.size()<j-i+1){ // There are duplicate characters
+//                 while(mp.size()<j-i+1){ // Remove characters from the left
+//                     mp[s[i]]--;
+//                     if(mp[s[i]] == 0){ // Remove character completely if count is 0
+//                         mp.erase(s[i]);
+//                     }
+//                     i++;
+//                 }
+//                 j++;
+//             }
+//         }
+//         return mx;
+//     }
 };
