6714f65d5f4457f7af561af6be2284a2f3458d48
pratikmandi <pratikmandi0984@gmail.com>
2024-07-14 20:42:21 +0530

    Q.347,Q.703 - Heap/Priority Queue Problems

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+     unordered_map<int,int> mpp;
+     for(auto x:nums){
+         mpp[x]++ ;
+     }
+
+    priority_queue<pair<int,int> > pq;
+    for(auto x:mpp) {
+        pq.push({x.second,x.first}); 
+    }
+
+    vector<int> ans ;
+    while(k--) {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+    return ans;
+    }
+};
